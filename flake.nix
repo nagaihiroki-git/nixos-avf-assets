bd587241bcdf6810252ed335d7d35f66f375400f
nagaihiroki-git <82458324+nagaihiroki-git@users.noreply.github.com>
2026-02-05 23:45:50 +0900

    Update `boot.kernelPackages` to use `linux-asahi` in `flake.nix` configuration

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
         # VM only - disable hardware drivers, use 16KB kernel only
         hardware.asahi.enable = false;
-        boot.kernelPackages = apple-silicon.packages.${system}.linux-asahi-pkg;
+        boot.kernelPackages = apple-silicon.packages.${system}.linux-asahi;
 
         # Boot & Console
         boot.kernelParams = [ "console=hvc0" "swiotlb=65536" ];
